d7723dae0df40e28de3bdaf2778c8ca0d3e1a04e
chiragkhimani <khimanichirag@gmail.com>
2021-09-09 06:13:40 +0530

    Master branch feature completed

---
+++ src/com/java/gitpractice/HomePage.java
@@ -0,0 +1,8 @@
+package com.java.gitpractice;
+
+public class HomePage {
+	public static void main(String[] args) {
+		System.out.println("logo");
+		System.out.println("header");
+	}
+}
---
+++ src/com/java/gitpractice/LoginPage.java
@@ -0,0 +1,9 @@
+package com.java.gitpractice;
+
+public class LoginPage {
+	public static void main(String[] args) {
+		System.out.println("username");
+		System.out.println("password");
+		System.out.println("login btn");
+	}
+}
